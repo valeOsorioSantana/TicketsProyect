0b2e5c6c3f0c6b981cb518aef3804bf9de262858
valeOsorioSantana <valeosoriosantana0@gmail.com>
2025-08-11 18:47:09 -0500

    Deploy 10

--- js/comprar.js
+++ js/comprar.js
@@ -436,5 +436,5 @@ document.getElementById("verMisTicketsBtn").addEventListener("click", () => {
 const btnVerEventos = document.querySelector('.btn-ver-eventos');
 
 btnVerEventos.addEventListener('click', () => {
-  window.location.href = '/index.html';
+  window.location.href = './index.html';
 });
