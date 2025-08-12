546a5b4e77d58e78a49a8f931a27c42d42405eb7
valeOsorioSantana <valeosoriosantana0@gmail.com>
2025-08-11 19:17:04 -0500

    Deploy 11

--- js/comprar.js
+++ js/comprar.js
@@ -396,7 +396,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         if (!res.ok) throw new Error(await res.text());
 
         // Redirigir a la página de información del evento
-        window.location.href = `/TicketsProyect/infoEvent.html?id=${eventId}`;
+        window.location.href = `${window.location.origin}/TicketLite/infoEvent.html?id=${eventId}`;
       } catch (err) {
         console.error("❌ Error al cancelar compra:", err.message);
         mensajePago.textContent = "❌ Error al cancelar compra: " + err.message;
